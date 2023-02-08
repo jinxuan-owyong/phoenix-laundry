69dfa694cd94a32ad9b510dcb13e10eef6a4e330
Jin Xuan Ow Yong <owyongjinxuan@gmail.com>
2023-02-08 19:27:12 +0800

    Update laundry config

--- lib/LaundryRoom/LaundryConfig.h
+++ lib/LaundryRoom/LaundryConfig.h
@@ -19,13 +19,13 @@ namespace laundry {
     };
 
     struct config_t {
-        int SCAN_INTERVAL = 100;     // time taken for each scan
-        int SCAN_THRESHOLD = 1600;   // threshold for LED state (12-bit ADC)
-        int SCAN_NUM_READINGS = 10;  // total readings to take
-        int THRESHOLD_CONSTANT = 7;
-        int THRESHOLD_BLINKING = 2;  // blink frequency = 1/second
-        int PIN_DRYER_A;             // ADC input pins
-        int PIN_DRYER_B;             // ADC 2 is reserved for WiFi, use ADC 1 pins
+        int SCAN_INTERVAL = 10;       // time taken for each scan
+        int SCAN_THRESHOLD = 1600;    // threshold for LED state (12-bit ADC)
+        int SCAN_NUM_READINGS = 100;  // total readings to take
+        int THRESHOLD_CONSTANT = 70;
+        int THRESHOLD_BLINKING = 15;  // blink frequency = 1/second
+        int PIN_DRYER_A;              // ADC input pins
+        int PIN_DRYER_B;              // ADC 2 is reserved for WiFi, use ADC 1 pins
         int PIN_WASHER_A;
         int PIN_WASHER_B;
         config_t() {}
